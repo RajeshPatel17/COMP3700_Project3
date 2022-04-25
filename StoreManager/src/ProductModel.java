bed156666315af04195cce2f0e74249da504103e
RajeshPatel17 <rajesh.patel20110@gmail.com>
2022-04-25 17:52:56 -0500

    cleared

--- StoreManager/src/ProductModel.java
+++ StoreManager/src/ProductModel.java
@@ -4,4 +4,8 @@ public class ProductModel {
     public String name;
     public double price;
     public double quantity;
+
+    public String toString(){
+        return "Product ID: " + productID + ", Name: " + name + ", Price: " + price + ", Quantity: " + quantity;
+    }
 }
